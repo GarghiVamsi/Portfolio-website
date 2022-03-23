13f207450995885f6b986f99a7ce53cc86e25bdd
Vamsi Garghi <vamsi@Vamsis-MacBook-Pro.local>
2022-03-23 00:09:38 -0500

    first commit on  portfolio website. Create a info component and added some functions to it.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Info from './Components/Intro page/Info';
+
+function App() {
+  return (
+    <div className="App">
+      <Info/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Intro page/Info.jsx
@@ -0,0 +1,25 @@
+import "./info.css"
+
+const Info = () => {
+    return (
+        <div className="info">
+            <div className="info-Left"> 
+            <div className="info-left-padding">
+                <h2 className="info-welcome"> Hello Welcome to my Webpage!</h2>
+                <h1 className="info-name"> My name is Vamsi Garghi</h1>
+                <div className="info-title">
+                    <div className="info-title-wrapper">
+                        <div className="info-title-item"> Bachelor's in Computer Science</div>
+                        <div className="info-title-item"> Data Analyst @ cognizant techonologies</div>
+                        <div className="info-title-item"> Avid basketball player</div>
+                        <div className="info-title-item"> Love Anime</div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div className="info-Right"> Right</div>
+        </div> 
+    )
+}
+
+export default Info
